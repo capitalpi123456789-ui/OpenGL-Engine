954457e8b87f811675f983d54551d988a5d903b6
Ayden Fisher <capitalpi123456789@gmail.com>
2026-06-22 19:44:39 -0700

    Add buffer classes, implemented VBO class

---
+++ include/classes/bufferClasses/VBO.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <glad/glad.h>
+
+class VBO
+{
+    public:
+
+        GLuint ID;
+
+        VBO();
+        ~VBO();
+
+        void bind();
+        void unbind();
+
+};
---
+++ src/classes/bufferClasses/VBO.cpp
@@ -0,0 +1,21 @@
+#include "classes/bufferClasses/VBO.h"
+
+VBO::VBO()
+{
+    glGenBuffers(1, &(this->ID));
+}
+
+VBO::~VBO()
+{
+    glDeleteBuffers(1, &this->ID);
+}
+
+void VBO::bind()
+{
+    glBindBuffer(GL_ARRAY_BUFFER, this->ID);
+}
+
+void VBO::unbind()
+{
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
